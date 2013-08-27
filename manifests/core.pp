4e734d0726622e9f06daa798f60ba724154019e6
Michael Chapman <michchap@cisco.com>
2013-08-27 16:20:39 +1000

    Remove nexus plugin from core
    This patch removes the nexus plugin
    setup from core. There is another patch
    to add this functionality to the coe
    module, where it will sit until it
    is of high enough quality to go upstream
    (probably havana release)

--- manifests/core.pp
+++ manifests/core.pp
@@ -227,6 +227,7 @@ class control(
   $cisco_vswitch_plugin              = $::cisco_vswitch_plugin,
   $cisco_nexus_plugin                = $::cisco_nexus_plugin,
   $nexus_credentials                 = $::nexus_credentials,
+  $nexus_config                      = $::nexus_config,
   # need to set from a variable
   # database
   $db_host                           = $::controller_node_address,
@@ -372,17 +373,10 @@ class control(
   if $cisco_nexus_plugin == 'nexus' {
     $cisco_nexus_plugin_real = 'quantum.plugins.cisco.nexus.cisco_nexus_plugin_v2.NexusPlugin'
 
-    package { 'python-ncclient':
-      ensure => installed,
-    } ~> Service['quantum-server']
-
-    # hack to make sure the directory is created
-    Quantum_plugin_cisco<||> ->
-    file {'/etc/quantum/plugins/cisco/nexus.ini':
-      owner => 'root',
-      group => 'root',
-      content => template('nexus.ini.erb')
-    } ~> Service['quantum-server']
+    class { 'coe::nexus':
+      nexus_credentials => $nexus_credentials,
+      nexus_config      => $nexus_config
+    }
   } else {
     $cisco_nexus_plugin_real = undef
   }
@@ -770,18 +764,4 @@ node master-node inherits "cobbler-node" {
 #  allow 192.168.0.0/24
 ',
     }
-}
-
-define nexus_creds {
-  $args = split($title, '/')
-  quantum_plugin_cisco_credentials {
-    "${args[0]}/username": value => $args[1];
-    "${args[0]}/password": value => $args[2];
-  }
-  exec {"${title}":
-    unless => "/bin/cat /var/lib/quantum/.ssh/known_hosts | /bin/grep ${args[0]}",
-    command => "/usr/bin/ssh-keyscan -t rsa ${args[0]} >> /var/lib/quantum/.ssh/known_hosts",
-    user    => 'quantum',
-    require => Package['quantum-server']
-  }
 }
